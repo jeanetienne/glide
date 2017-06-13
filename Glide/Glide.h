897b5eb9151e35fd8179b4e7c01371b24df60bd0
Jean-Étienne <git@jeanetienne.net>
2017-06-13 10:19:03 +1000

    Basic Xcode project setup

---
+++ Glide/Glide.h
@@ -0,0 +1,16 @@
+//
+// Glide
+// Copyright © 2017 Jean-Étienne. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Glide.
+FOUNDATION_EXPORT double GlideVersionNumber;
+
+//! Project version string for Glide.
+FOUNDATION_EXPORT const unsigned char GlideVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Glide/PublicHeader.h>
+
+
