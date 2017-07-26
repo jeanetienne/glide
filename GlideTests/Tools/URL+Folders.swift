23fb02373deea2e93b758609c4aa4e160c6b13f9
Jean-Étienne <git@jeanetienne.net>
2017-07-26 23:00:28 +1000

    Adding tooling for tests

--- GlideTests/Tools/URL+Folders.swift
+++ GlideTests/Tools/URL+Folders.swift
@@ -7,8 +7,13 @@ import Foundation
 
 extension URL {
 
-    static var moviesFolder: URL = { () -> URL in
+    static var moviesFolder: URL {
         return FileManager.default.urls(for: .moviesDirectory, in: .userDomainMask)[0]
-    }()
+    }
+
+    static var cacheFolder: URL {
+        let folderPath = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask)[0]
+        return folderPath.appendingPathComponent(Bundle.tests.bundleIdentifier!)
+    }
 
 }
